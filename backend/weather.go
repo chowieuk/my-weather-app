96929620b7223a94e5cfdb7072b7a2a1f4e77d11
chowie <christopher.howie@gmail.com>
2023-06-22 14:25:27 -0700

    initial commit

---
+++ backend/weather.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type WeatherStackManager struct {
+	BaseUrl     string
+	WEATHER_KEY string
+	Client      http.Client
+}
+
+type WeatherApiResponse struct {
+	Location Location `json:"location"`
+	Current  Current  `json:"current"`
+}
+
+type Location struct {
+	Name    string `json:"name"`
+	Country string `json:"country"`
+	Region  string `json:"region"`
+}
+
+type Current struct {
+	Temperature int `json:"temperature"`
+}
+
+type ApiErrorResponse struct {
+	Success bool     `json:"success"`
+	Error   ApiError `json:"error"`
+}
+
+type ApiError struct {
+	Code int    `json:"code"`
+	Type string `json:"type"`
+	Info string `json:"info"`
+}
+
+func (wsm *WeatherStackManager) GetWeather(location string) (*WeatherApiResponse, error) {
+	url := fmt.Sprintf("%s/current?access_key=%s&query=%s", wsm.BaseUrl, wsm.WEATHER_KEY, location)
+	response, err := wsm.Client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		var apiErrorResponse ApiErrorResponse
+		err = json.Unmarshal(body, &apiErrorResponse)
+		if err != nil {
+			return nil, err
+		}
+		log.Print("error: ", &apiErrorResponse)
+		return nil, fmt.Errorf("API error occurred: code=%d, type=%s, info=%s", apiErrorResponse.Error.Code, apiErrorResponse.Error.Type, apiErrorResponse.Error.Info)
+	}
+
+	var weatherResponse WeatherApiResponse
+	err = json.Unmarshal(body, &weatherResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Print(&weatherResponse)
+	return &weatherResponse, nil
+}
+
+func main() {
+
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the WEATHER_KEY from the environment variables
+	WEATHER_KEY := os.Getenv("WEATHER_KEY")
+	wsm := WeatherStackManager{
+		BaseUrl:     "http://api.weatherstack.com/",
+		WEATHER_KEY: WEATHER_KEY,
+	}
+
+	data, err := wsm.GetWeather("Vancouver")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(data)
+}
---
+++ backend/weather_test.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+func TestWeatherStackManager_GetWeather(t *testing.T) {
+
+	type fields struct {
+		BaseUrl     string
+		WEATHER_KEY string
+		Client      http.Client
+	}
+	type args struct {
+		location string
+	}
+
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the WEATHER_KEY from the environment variables
+	WEATHER_KEY := os.Getenv("WEATHER_KEY")
+
+	// // grab example JSON response from file
+	// file, err := os.Open("./test_mocks/weatherCurrentResponse.json")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+
+	// create a new server serving the appropriate mock JSON
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		location := query.Get("query")
+		key := query.Get("access_key")
+
+		var mockResponse []byte
+		var err error
+		var statusCode int
+
+		switch {
+		case key == "":
+			mockResponse, err = os.ReadFile("./test_mocks/101_missing_access_key.json")
+			statusCode = 401
+		case key == "invalid":
+			mockResponse, err = os.ReadFile("./test_mocks/101_invalid_access_key.json")
+			statusCode = 401
+		case location == "":
+			mockResponse, err = os.ReadFile("./test_mocks/601_missing_query.json")
+			statusCode = 401
+		case location == "602_no_results":
+			mockResponse, err = os.ReadFile("./test_mocks/602_no_results.json")
+			statusCode = 400
+		case location == "104_usage_limit_reached":
+			mockResponse, err = os.ReadFile("./test_mocks/104_usage_limit_reached.json")
+			statusCode = 400
+		default:
+			mockResponse, err = os.ReadFile("./test_mocks/weatherCurrentResponse.json")
+			statusCode = 200
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(statusCode)
+		_, _ = w.Write(mockResponse)
+	}))
+	defer server.Close()
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *WeatherApiResponse
+		wantErr bool
+	}{
+		{
+			name: "successful case",
+			fields: fields{
+				BaseUrl:     server.URL,
+				WEATHER_KEY: WEATHER_KEY,
+			},
+			args: args{
+				location: "Vancouver",
+			},
+			want: &WeatherApiResponse{
+				Location: Location{
+					Name:    "Vancouver",
+					Country: "Canada",
+					Region:  "British Columbia",
+				},
+				Current: Current{
+					Temperature: 19,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "101_missing_access_key error",
+			fields: fields{
+				BaseUrl:     server.URL,
+				WEATHER_KEY: "",
+			},
+			args: args{
+				location: "Vancouver",
+			},
+			wantErr: true,
+		},
+		{
+			name: "101_invalid_access_key error",
+			fields: fields{
+				BaseUrl:     server.URL,
+				WEATHER_KEY: "invalid",
+			},
+			args: args{
+				location: "Vancouver",
+			},
+			wantErr: true,
+		},
+		{
+			name: "601_missing_query error",
+			fields: fields{
+				BaseUrl:     server.URL,
+				WEATHER_KEY: WEATHER_KEY,
+			},
+			args: args{
+				location: "", // this empty string triggers the 601 error in our mock server
+			},
+			wantErr: true,
+		},
+		{
+			name: "602_no_results error",
+			fields: fields{
+				BaseUrl:     server.URL,
+				WEATHER_KEY: WEATHER_KEY,
+			},
+			args: args{
+				location: "602_no_results", // this string triggers the 602 error in our mock server
+			},
+			wantErr: true,
+		},
+		{
+			name: "104_usage_limit_reached error",
+			fields: fields{
+				BaseUrl:     server.URL,
+				WEATHER_KEY: WEATHER_KEY,
+			},
+			args: args{
+				location: "104_usage_limit_reached", // this string triggers the 104 error in our mock server
+			},
+
+			wantErr: true,
+		},
+		{
+			name: "server error",
+			fields: fields{
+				BaseUrl:     "https://fake.unresolved",
+				WEATHER_KEY: WEATHER_KEY,
+			},
+			args: args{
+				location: "any location",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			wsm := &WeatherStackManager{
+				BaseUrl:     tt.fields.BaseUrl,
+				WEATHER_KEY: tt.fields.WEATHER_KEY,
+				Client:      tt.fields.Client,
+			}
+			got, err := wsm.GetWeather(tt.args.location)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WeatherStackManager.GetWeather() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.want != nil && (got.Location != tt.want.Location || got.Current != tt.want.Current) {
+				t.Errorf("WeatherStackManager.GetWeather() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
