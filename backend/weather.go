ecc7c44369f8e13a77d84dda3f581b7015bb2890
chowie <christopher.howie@gmail.com>
2023-06-26 11:49:07 -0700

    began working on astrological data selection and serving

--- backend/weather.go
+++ backend/weather.go
@@ -15,11 +15,13 @@ type WeatherStackManager struct {
 	BaseUrl     string
 	WEATHER_KEY string
 	Client      http.Client
+	Cache       map[string]Astro
 }
 
 type WeatherApiResponse struct {
-	Location Location `json:"location"`
-	Current  Current  `json:"current"`
+	Location Location            `json:"location"`
+	Current  Current             `json:"current"`
+	Forecast map[string]Forecast `json:"forecast"`
 }
 
 type Location struct {
@@ -33,6 +35,27 @@ type Current struct {
 	Fahrenheit  int `json:"fahrenheit"`
 }
 
+type Forecast struct {
+	Date      string  `json:"date"`
+	DateEpoch int64   `json:"date_epoch"`
+	Astro     Astro   `json:"astro"`
+	MinTemp   int     `json:"mintemp"`
+	MaxTemp   int     `json:"maxtemp"`
+	AvgTemp   int     `json:"avgtemp"`
+	TotalSnow int     `json:"totalsnow"`
+	SunHour   float64 `json:"sunhour"`
+	UVIndex   int     `json:"uv_index"`
+}
+
+type Astro struct {
+	Sunrise          string `json:"sunrise"`
+	Sunset           string `json:"sunset"`
+	Moonrise         string `json:"moonrise"`
+	Moonset          string `json:"moonset"`
+	MoonPhase        string `json:"moon_phase"`
+	MoonIllumination int    `json:"moon_illumination"`
+}
+
 type ApiErrorResponse struct {
 	Success bool     `json:"success"`
 	Error   ApiError `json:"error"`
@@ -42,6 +65,10 @@ type ApiError struct {
 	Code int    `json:"code"`
 	Type string `json:"type"`
 	Info string `json:"info"`
+}
+
+func makeKey(location string, date string) string {
+	return fmt.Sprintf("%s:%s", location, date)
 }
 
 func (wsm *WeatherStackManager) GetWeather(location string) (*WeatherApiResponse, error) {
@@ -80,8 +107,86 @@ func (wsm *WeatherStackManager) GetWeather(location string) (*WeatherApiResponse
 	return &weatherResponse, nil
 }
 
+func (wsm *WeatherStackManager) GetAstro(location string, date string) (Astro, error) {
+
+	// Check the cache first
+	key := makeKey(location, date)
+	cachedData, found := wsm.Cache[key]
+	if found {
+		return cachedData, nil
+	}
+
+	url := fmt.Sprintf("%s/forecast?access_key=%s&query=%s", wsm.BaseUrl, wsm.WEATHER_KEY, location)
+	response, err := wsm.Client.Get(url)
+	if err != nil {
+		return Astro{}, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return Astro{}, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		var apiErrorResponse ApiErrorResponse
+		err = json.Unmarshal(body, &apiErrorResponse)
+		if err != nil {
+			return Astro{}, err
+		}
+		log.Printf("%+v\n", &apiErrorResponse)
+		return Astro{}, fmt.Errorf("API error occurred: code=%d, type=%s, info=%s", apiErrorResponse.Error.Code, apiErrorResponse.Error.Type, apiErrorResponse.Error.Info)
+	}
+
+	var forecastResponse WeatherApiResponse
+	err = json.Unmarshal(body, &forecastResponse)
+	if err != nil {
+		return Astro{}, err
+	}
+
+	forecastData := forecastResponse.Forecast[date]
+	astroData := forecastData.Astro
+	wsm.Cache[makeKey(location, date)] = astroData
+
+	return astroData, nil
+}
+
 func CelsiusToFahrenheit(celsius int) int {
 	return int(float64(celsius)*1.8 + 32)
+}
+
+type WeatherManager interface {
+	GetAstro(location string, date string) (Astro, error)
+}
+
+func AstroHandler(w http.ResponseWriter, r *http.Request, wm WeatherManager) {
+	// Parse location and date from request's query parameters
+	location := r.URL.Query().Get("location")
+	date := r.URL.Query().Get("date")
+
+	if location == "" || date == "" {
+		http.Error(w, "Missing 'location' or 'date' query parameter", http.StatusBadRequest)
+		return
+	}
+
+	// Get the astro data
+	astroData, err := wm.GetAstro(location, date)
+	if err != nil {
+		http.Error(w, "Failed to get astro data", http.StatusInternalServerError)
+		return
+	}
+
+	// Send the astro data as response
+	// Convert the astroData to JSON before sending it
+	astroDataJson, err := json.Marshal(astroData)
+	if err != nil {
+		http.Error(w, "Failed to convert astro data to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(astroDataJson)
 }
 
 func main() {
@@ -96,13 +201,15 @@ func main() {
 	WEATHER_KEY := os.Getenv("WEATHER_KEY")
 
 	wsm := WeatherStackManager{
-		BaseUrl:     "http://api.weatherstack.com/",
+		BaseUrl:     "http://api.weatherstack.com",
 		WEATHER_KEY: WEATHER_KEY,
+		Cache:       make(map[string]Astro),
 	}
 
-	data, err := wsm.GetWeather("Vancouver")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(data)
+	// Wrap the AstroHandler in a function that matches the expected signature
+	http.HandleFunc("/astro", func(w http.ResponseWriter, r *http.Request) {
+		AstroHandler(w, r, &wsm)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- backend/weather_test.go
+++ backend/weather_test.go
@@ -5,10 +5,24 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/joho/godotenv"
 )
+
+type MockWeatherManager struct{}
+
+func (mwm *MockWeatherManager) GetAstro(location string, date string) (Astro, error) {
+	return Astro{
+		Sunrise:          "05:07 AM",
+		Sunset:           "09:22 PM",
+		Moonrise:         "06:58 AM",
+		Moonset:          "11:52 PM",
+		MoonPhase:        "Waxing Crescent",
+		MoonIllumination: 3,
+	}, nil
+}
 
 func TestWeatherStackManager_GetWeather(t *testing.T) {
 
@@ -195,5 +209,239 @@ func TestCelsiusToFahrenheit(t *testing.T) {
 	result = CelsiusToFahrenheit(100)
 	if result != 212 {
 		t.Errorf("Expected 212, but got %d", result)
+	}
+}
+
+func TestWeatherStackManager_GetAstro(t *testing.T) {
+	type fields struct {
+		BaseUrl     string
+		WEATHER_KEY string
+		Client      http.Client
+		Cache       map[string]Astro
+	}
+	type args struct {
+		location string
+		date     string
+	}
+
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the WEATHER_KEY from the environment variables
+	WEATHER_KEY := os.Getenv("WEATHER_KEY")
+
+	// create a new server serving the appropriate mock JSON
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		location := query.Get("query")
+		key := query.Get("access_key")
+
+		var mockResponse []byte
+		var err error
+		var statusCode int
+
+		switch {
+		case key == "":
+			mockResponse, err = os.ReadFile("./test_mocks/101_missing_access_key.json")
+			statusCode = 401
+		case key == "invalid":
+			mockResponse, err = os.ReadFile("./test_mocks/101_invalid_access_key.json")
+			statusCode = 401
+		case location == "":
+			mockResponse, err = os.ReadFile("./test_mocks/601_missing_query.json")
+			statusCode = 401
+		case location == "602_no_results":
+			mockResponse, err = os.ReadFile("./test_mocks/602_no_results.json")
+			statusCode = 400
+		case location == "104_usage_limit_reached":
+			mockResponse, err = os.ReadFile("./test_mocks/104_usage_limit_reached.json")
+			statusCode = 400
+		default:
+			mockResponse, err = os.ReadFile("./test_mocks/weatherForecastResponse.json")
+			statusCode = 200
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(statusCode)
+		_, _ = w.Write(mockResponse)
+	}))
+	defer server.Close()
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    Astro
+		wantErr bool
+	}{
+		{
+			name: "successful case",
+			fields: fields{
+				BaseUrl:     server.URL,
+				WEATHER_KEY: WEATHER_KEY,
+			},
+			args: args{
+				location: "Vancouver",
+				date:     "2023-06-20",
+			},
+			want: Astro{
+				Sunrise:          "05:07 AM",
+				Sunset:           "09:22 PM",
+				Moonrise:         "06:58 AM",
+				Moonset:          "11:52 PM",
+				MoonPhase:        "Waxing Crescent",
+				MoonIllumination: 3,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			wsm := &WeatherStackManager{
+				BaseUrl:     tt.fields.BaseUrl,
+				WEATHER_KEY: tt.fields.WEATHER_KEY,
+				Client:      tt.fields.Client,
+				Cache:       make(map[string]Astro),
+			}
+			got, err := wsm.GetAstro(tt.args.location, tt.args.date)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WeatherStackManager.GetForecast() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("WeatherStackManager.GetForecast() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWeatherStackManager_CachingBehavior(t *testing.T) {
+
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the WEATHER_KEY from the environment variables
+	WEATHER_KEY := os.Getenv("WEATHER_KEY")
+
+	requestCount := 0 // Create a counter variable
+
+	// create a new server serving the appropriate mock JSON
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestCount++
+		query := r.URL.Query()
+		location := query.Get("query")
+		key := query.Get("access_key")
+
+		var mockResponse []byte
+		var err error
+		var statusCode int
+
+		switch {
+		case key == "":
+			mockResponse, err = os.ReadFile("./test_mocks/101_missing_access_key.json")
+			statusCode = 401
+		case key == "invalid":
+			mockResponse, err = os.ReadFile("./test_mocks/101_invalid_access_key.json")
+			statusCode = 401
+		case location == "":
+			mockResponse, err = os.ReadFile("./test_mocks/601_missing_query.json")
+			statusCode = 401
+		case location == "602_no_results":
+			mockResponse, err = os.ReadFile("./test_mocks/602_no_results.json")
+			statusCode = 400
+		case location == "104_usage_limit_reached":
+			mockResponse, err = os.ReadFile("./test_mocks/104_usage_limit_reached.json")
+			statusCode = 400
+		default:
+			mockResponse, err = os.ReadFile("./test_mocks/weatherForecastResponse.json")
+			statusCode = 200
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(statusCode)
+		_, _ = w.Write(mockResponse)
+	}))
+	defer server.Close()
+
+	wsm := WeatherStackManager{
+		BaseUrl:     server.URL,
+		WEATHER_KEY: WEATHER_KEY,
+		Cache:       make(map[string]Astro),
+	}
+
+	// Call GetAstro to populate the cache
+	firstAstro, err := wsm.GetAstro("Vancouver", "2023-06-20")
+	if err != nil {
+		t.Fatalf("Unexpected error during GetAstro: %v", err)
+	}
+
+	// Call GetAstro again and check that it returns the same data
+	secondAstro, err := wsm.GetAstro("Vancouver", "2023-06-20")
+	if err != nil {
+		t.Fatalf("Unexpected error during second GetAstro: %v", err)
+	}
+	if !reflect.DeepEqual(firstAstro, secondAstro) {
+		t.Errorf("Second GetAstro returned different data: first %v, second %v", firstAstro, secondAstro)
+	}
+
+	// Check that only one request was made to the server
+	if requestCount != 1 {
+		t.Errorf("Expected 1 request to the server, but got %d", requestCount)
+	}
+
+	// Verify the cache directly
+	cacheKey := "Vancouver:2023-06-20"
+	cachedData, found := wsm.Cache[cacheKey]
+	if !found {
+		t.Errorf("No data found in cache for key: %s", cacheKey)
+	}
+
+	// Compare cachedAstro with the expected data
+	expectedAstro := Astro{
+		Sunrise:          "05:07 AM",
+		Sunset:           "09:22 PM",
+		Moonrise:         "06:58 AM",
+		Moonset:          "11:52 PM",
+		MoonPhase:        "Waxing Crescent",
+		MoonIllumination: 3,
+	}
+
+	if !reflect.DeepEqual(cachedData, expectedAstro) {
+		t.Errorf("Cached data is not as expected. Got %v, want %v", cachedData, expectedAstro)
+	}
+}
+
+func TestAstroHandler(t *testing.T) {
+
+	mwm := &MockWeatherManager{}
+
+	req, err := http.NewRequest("GET", "/astro?location=Vancouver&date=2023-06-20", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	AstroHandler(rr, req, mwm)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := `{"sunrise":"05:07 AM","sunset":"09:22 PM","moonrise":"06:58 AM","moonset":"11:52 PM","moon_phase":"Waxing Crescent","moon_illumination":3}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
